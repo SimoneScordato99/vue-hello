f1feb2f59f20d51a7961c60e991244f87733511f
SimoneScordato99 <scordato.simone@yahoo.com>
2023-04-18 12:59:44 +0200

    add click and img

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ createApp({
   data() {
     return {
       message: 'Ciao sono Simone!!!',
-      scrittura: ""
+      scrittura: "",
+      immagine: "https://www.adesso-online.de/sites/adesso.spotlight-verlag.de/files/2018-10/ciao_compleanno.jpg"
     }
   },
   methods: {
     scrivi(){
         this.scrittura= "Ciaooooooo"
-    }
+    },
+    
   },
 }).mount('#app')
